fd50e62e716f2c74ea2d874bd7bee9e1971ea87a
Doğukan Ürker <dogukanurker@icloud.com>
2024-06-15 23:47:07 +0300

    feat: add test app

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("arkligCSS");
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href =
+  "https://cdn.jsdelivr.net/gh/DogukanUrker/ArkligCSS@main/ArkligMin/Arklig.min.css";
+document.head.appendChild(link);
+
+const selectElement = document.getElementById("color");
+selectElement.addEventListener("change", function () {
+  const selectedValue = selectElement.value;
+  if (selectedValue != "default") {
+    link.rel = "stylesheet";
+    link.href = `https://cdn.jsdelivr.net/gh/DogukanUrker/ArkligCSS@main/ArkligMin/Arklig.${selectedValue}.min.css`;
+    document.head.appendChild(link);
+  } else {
+    link.rel = "stylesheet";
+    link.href =
+      "https://cdn.jsdelivr.net/gh/DogukanUrker/ArkligCSS@main/ArkligMin/Arklig.min.css";
+    document.head.appendChild(link);
+  }
+});
+
+const selectColorScheme = document.getElementById("theme");
+selectColorScheme.addEventListener("change", function () {
+  const selectedValue = selectColorScheme.value;
+  if (selectedValue === "auto") {
+    document.documentElement.style.colorScheme = "light dark";
+  } else if (selectedValue === "light") {
+    document.documentElement.style.colorScheme = "light";
+  } else if (selectedValue === "dark") {
+    document.documentElement.style.colorScheme = "dark";
+  }
+});
